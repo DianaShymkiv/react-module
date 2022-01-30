6eb8bc6f74f6a906580f7ec1392423f82b26c6dd
DianaShymkiv <shymkiv.diana@gmail.com>
2022-01-30 21:41:59 +0000

    classwork 4

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
 import './App.css';
 
-import {useReducer} from "react";
+import {useReducer, useState} from "react";
+import {Cat} from "./components/Cat/Cat";
+import {Dog} from "./components/Dog/Dog";
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'inc':
-            return {...state, [`count${action.payload}`]: state[`count${action.payload}`] + 1}
-        case 'dec':
-            return {...state, [`count${action.payload}`]: state[`count${action.payload}`] - 1}
-        case 'reset':
-            return {...state, [`count${action.payload}`]: 0}
+        case 'cat':
+            if (!action.form.cat) {
+                throw new Error('ERROR');
+            }
+            return {...state, cats: [...state.cats, action.form.cat]};
+        case 'dog':
+            if (!action.form.dog) {
+                throw new Error('ERROR');
+            }
+            return {...state, dogs: [...state.dogs, action.form.dog]};
+        case 'deleteCat':
+            return {...state, cats: state.cats.filter((x, i) => i !== action.index)};
+        case 'deleteDog':
+            return {...state, dogs: state.dogs.filter((x, i) => i !== action.index)};
         default:
-            throw new Error('ERROR')
+            throw new Error('ERROR');
     }
 }
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
+    const [form, setForm] = useState({cat: '', dog: ''});
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({type: e.target[0].name, form: form})
+    }
+
+    const onChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value});
+    }
+
+    const deleteAnimal = (e, type, index) => {
+        e.preventDefault();
+        dispatch({type: type, index: index});
+    }
+
     return (
         <div>
-            <div>
-                <div>{state.count1}</div>
-                <button onClick={() => dispatch({type: 'inc', payload: 1})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec', payload: 1})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset', payload: 1})}>RESET</button>
-            </div>
-            <div>
-                <div>{state.count2}</div>
-                <button onClick={() => dispatch({type: 'inc', payload: 2})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec', payload: 2})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset', payload: 2})}>RESET</button>
-            </div>
-            <div>
-                <div>{state.count3}</div>
-                <button onClick={() => dispatch({type: 'inc', payload: 3})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec', payload: 3})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset', payload: 3})}>RESET</button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Add Cat: <input type="text" name='cat' value={form.cat} onChange={onChange}/></label>
+                    <button>Save</button>
+                </div>
+            </form>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Add Dog: <input type="text" name='dog' value={form.dog} onChange={onChange}/></label>
+                    <button>Save</button>
+                </div>
+            </form>
+            <hr/>
+            <div className={'flex'}>
+                <div>
+                    {state.cats && state.cats.map(((value, index) => <Cat key={index} cat={value} index={index}
+                                                                          deleteAnimal={deleteAnimal}/>))}
+                </div>
+                <div>
+                    {state.dogs && state.dogs.map(((value, index) => <Dog key={index} dog={value} index={index}
+                                                                          deleteAnimal={deleteAnimal}/>))}
+                </div>
             </div>
         </div>
     );
